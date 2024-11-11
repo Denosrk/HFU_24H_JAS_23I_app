bd2c42c056f03b9a787004ec418ec60672fef1e5
Deniz Sirikli <deniz.sirikli@hfu.ch>
2024-11-11 21:06:09 +0100

    objekt test eingefügt array test eingefügt

--- JS/sum.test.js
+++ JS/sum.test.js
@@ -3,3 +3,15 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('objekt test', () => {
+    const a = {name:"bob"}
+
+    expect(a.name).toBe("bob");
+  });
+
+test('array test', () => {
+    const a = [1, 2, 3];
+
+    expect(a[0]).toBe(1);
+  });
