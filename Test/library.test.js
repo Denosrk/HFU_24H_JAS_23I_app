030a098d624c97a962b65040cd72b00cbe1442b8
Deniz Sirikli <deniz.sirikli@hfu.ch>
2024-12-02 20:38:33 +0100

    Aktueller stand

--- Test/library.test.js
+++ Test/library.test.js
@@ -1,21 +1,29 @@
-import { test, describe } from '@jest/globals';
-import { counter, increment, decrement } from './library';
+import "isomorphic-fetch";  // Oben in die Datei
+import { test, describe, expect } from "@jest/globals";
+import { counter, increment, decrement } from "./library";
 
-describe('library', () => {
-  test('counter is initialized to 1', () => {
+describe("library", () => {
+  test("counter is initialized to 1", () => {
     expect(counter()).toBe(1);
   });
 
-  test('increment increases by 1', () => {
+  test("increment increases by 1", () => {
     const currentCounter = counter();
 
     expect(increment()).toBe(currentCounter + 1);
   });
 
-  test('decrement decreases by 1', () => {
+  test("decrement decreases by 1", () => {
     const currentCounter = counter();
 
     expect(decrement()).toBe(currentCounter - 1);
   });
-})
 
+  test("call fetch gets addresses", () => {
+    return fetch("http://localhost:3000/addresses")
+      .then(data => data.json())
+      .then(addresses => {
+        expect(addresses.length).toBe(3);
+      });
+  });
+});
